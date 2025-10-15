31878ec847b5b569a778626729d148594f542406
trandy92 <andreas_reichel@online.de>
2025-10-15 12:25:36 +0200

    progress

--- src/client.cpp
+++ src/client.cpp
@@ -3,8 +3,6 @@
 
 #include "TcpConnection.h"
 
-using boost::asio::ip::tcp;
-
 int main()
 {
   try
@@ -12,12 +10,8 @@ int main()
     boost::asio::io_context io;
     auto tcpConnection = TcpConnection::create(io);
     tcpConnection->connect();
-    tcpConnection->listenForIncomingMessages(
-        [](const std::string& msg)
-        {
-          auto x = msg;
-          std::cout << "[Incoming] " << msg << std::endl;
-        });
+    tcpConnection->listenForIncomingMessages([](const std::string& msg)
+                                             { std::cout << "[Incoming] " << msg << std::endl; });
     std::jthread ioThread([&]() { io.run(); });
 
     std::cout << "Start chatting" << std::endl;
